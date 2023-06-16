377c88c3202a2d0e4e3ca7d0af7071eb051f2ebd
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-06-16 15:19:06 +0200

    DataInput class changed to data class

--- src/main/kotlin/org/maurycy/framework/mba/model/DataInput.kt
+++ src/main/kotlin/org/maurycy/framework/mba/model/DataInput.kt
@@ -1,5 +1,3 @@
 package org.maurycy.framework.mba.model
 
-import java.io.Serializable
-
-class DataInput(var id: String, var type: String, var data: Map<String, String>) : Serializable
+data class DataInput(val id: String, val type: String, val data: Map<String, String>)
